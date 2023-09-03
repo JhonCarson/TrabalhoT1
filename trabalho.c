fbb2b230a19ab4b04a89cf877c5e94f848c020d1
JhonCarson <140865490+JhonCarson@users.noreply.github.com>
2023-09-03 11:37:17 -0300

    Atualização Trabalho

--- trabalho.c
+++ trabalho.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+//Estrutura para armazenar o endereço
+struct endereco {
+    char rua [30];
+    int num;
+    char bairro [30];
+    char cidade [40];
+    char estado [40];
+};
+
+struct materia {
+    char nome[30];
+    float media;
+};
+
 
 // Estrutura para armazenar informações de um aluno
 struct Aluno {
     char nome[50];
     int ra;
     char dataNascimento[11]; // Formato: dd/mm/yyyy
-    char endereco1[100];
-    char endereco2[100];
-    float notas[5];
+    struct endereco endereco[2];
+    struct materia materia[5];
 };
+
+//Imprimir a Struct
+
+void listarAlunos(struct Aluno aluno) {
+
+    printf("\nNome do Aluno: %s", aluno.nome);
+    printf("\nRA do Aluno: %d", aluno.ra);
+    printf("\nData de Nascimento do Aluno: %s", aluno.dataNascimento);
+    printf("\nEndereco 1 do Aluno: %s", aluno.endereco);
+    printf("\nEndereco 2 do Aluno: %s", aluno.endereco);
+
+    /*for (int i = 0; i < 5; i++) {
+    
+        printf("\nNome da Materia: %s e Media %.2f\n", materia[i].nome, materia[i].media);
+    }*/
+    
+}
 
 // Função para encontrar um aluno pelo RA
 int encontrarAluno(struct Aluno alunos[], int ra, int tamanho) {
@@ -33,40 +65,79 @@ int main() {
         printf("3. Mostrar informacoes de um aluno pelo RA\n");
         printf("4. Mostrar informacoes de todos os alunos\n");
         printf("5. Sair\n");
-        printf("Escolha a operacao (1-5): ");
+        printf("\nEscolha a operacao (1-5): ");
         scanf("%d", &opcao);
+        printf("\n ");
 
         switch (opcao) {
             case 1:
                 if (numAlunos < 20) {
                     struct Aluno novoAluno;
-                    printf("Informe o nome do aluno: ");
-                    scanf("%s", novoAluno.nome);
-                    printf("Informe o RA do aluno: ");
+                    printf("\nInforme o nome do aluno: ");
+                    setbuf(stdin, NULL); // Limpar o buffer de entrada
+                    fgets(novoAluno.nome, sizeof(novoAluno.nome), stdin);
+                    
+                    printf("\nInforme o RA do aluno: ");
                     scanf("%d", &novoAluno.ra);
 
                     // Verifica se o RA já está armazenado
                     if (encontrarAluno(alunos, novoAluno.ra, numAlunos) == -1) {
-                        printf("Informe a data de nascimento (dd/mm/yyyy): ");
-                        scanf("%s", novoAluno.dataNascimento);
-                        printf("Informe o primeiro endereco (rua, numero, bairro, cidade, estado): ");
-                        scanf("%s", novoAluno.endereco1);
-                        printf("Informe o segundo endereco (rua, numero, bairro, cidade, estado): ");
-                        scanf("%s", novoAluno.endereco2);
-                        printf("Informe as notas das 5 disciplinas: ");
+ 
+                        printf("\nInforme a data de nascimento (dd/mm/yyyy): ");
+                        scanf("%s", &novoAluno.dataNascimento);
+
+                    //Preencher Endereços
+                        for (int i = 0; i < 2; i++) {
+                            
+                            setbuf(stdin, NULL);
+
+                            printf("\nInforme a rua do endereco %d: ", i+1);
+                            fgets(novoAluno.endereco[i].rua, sizeof(novoAluno.endereco[i].rua), stdin);
+                        
+                            printf("\nInforme o numero do endereco %d: ", i+1);
+                            scanf("%d", &novoAluno.endereco[i].num);
+
+                            setbuf(stdin, NULL);
+
+                            printf("\nInforme o bairro do endereco %d: ", i+1);
+                            fgets(novoAluno.endereco[i].bairro, sizeof(novoAluno.endereco[i].bairro), stdin);
+
+                            setbuf(stdin, NULL);
+                        
+                            printf("\nInforme a cidade do endereco %d: ", i+1);
+                            fgets(novoAluno.endereco[i].cidade, sizeof(novoAluno.endereco[i].cidade), stdin);   
+
+                            setbuf(stdin, NULL);                   
+
+                            printf("\nInforme o estado do endereco %d: ", i+1);
+                            fgets(novoAluno.endereco[i].estado, sizeof(novoAluno.endereco[i].estado), stdin);      
+                        }                                    
+
+                        /*printf("Informe o primeiro endereco: ");
+                        scanf("%s", &novoAluno.endereco[0]);
+                        printf("Informe o segundo endereco: ");
+                        scanf("%s", &novoAluno.endereco[1]);*/
+
                         for (int i = 0; i < 5; i++) {
-                            scanf("%f", &novoAluno.notas[i]);
+                            
+                            setbuf(stdin, NULL);    
+
+                            printf("\nInforme o nome da disciplina %d: ", i+1);
+                            fgets(novoAluno.materia[i].nome, sizeof(novoAluno.materia[i].nome), stdin);
+
+                            printf("\nInforme a media da disciplina %d: ", i+1);
+                            scanf(" %f", &novoAluno.materia[i].media);
                         }
 
                         // Adiciona o novo aluno ao vetor de alunos
                         alunos[numAlunos] = novoAluno;
                         numAlunos++;
-                        printf("Aluno adicionado com sucesso!\n");
+                        printf("\nAluno adicionado com sucesso!\n");
                     } else {
-                        printf("O RA ja esta armazenado. Nao foi possivel adicionar o aluno.\n");
+                        printf("\nO RA ja esta armazenado. Nao foi possivel adicionar o aluno.\n");
                     }
                 } else {
-                    printf("Nao ha espaco para adicionar mais alunos.\n");
+                    printf("\nNao ha espaco para adicionar mais alunos.\n");
                 }
                 break;
 
@@ -79,15 +150,21 @@ int main() {
                 break;
 
             case 4:
-                // Implemente a operacao de mostrar informacoes de todos os alunos aqui
+                //4. Mostrar informacoes de todos os alunos
+
+                for (int i = 0; i < numAlunos; i++) {
+                
+                    listarAlunos(alunos[i]);
+                }
+
                 break;
 
             case 5:
-                printf("Saindo da aplicacao. Ate logo!\n");
+                printf("\nSaindo da aplicacao. Ate logo!\n");
                 break;
 
             default:
-                printf("Opcao invalida. Tente novamente.\n");
+                printf("\nOpcao invalida. Tente novamente.\n");
         }
     } while (opcao != 5);
 
